b5d812f4f3824c301529e27ed8949738b4afa9d9
blazikenmask <blazikenmask@gmail.com>
2014-09-28 20:27:59 +0530

    second upload: string

--- lib/codestring.cpp
+++ lib/codestring.cpp
@@ -1,5 +1,4 @@
 #include "codestring.h"
-
 /**
 * Basic functions.
 *	EDITING THESE CHANGES THE FRAMEWORK OF THE TEMPLATE
@@ -10,12 +9,16 @@
 *	 but are actually objects of the String object .
 **/
 
+/*******************************
+* Constructors and operator =
+*******************************/
+
 // empty constructor.
 String::String(){
 	len = 0;
 }
 
-// copy contructors.
+// copy contructors : assignment is copy
 String::String( String& s){
 	len = s.length();
 	data = new char[len];
@@ -31,7 +34,25 @@ String::String( const char *s){
 							   // so dont use strcpy()
 }
 
-// Destructor : free some space, for fun
+/** operator = **/
+String& String::operator=( String& s){
+	len = s.length();
+	//delete[] data;
+	data = new char[len];
+	for(int i=0;i<len;i++)
+		data[i] = s.charAt(i);
+	return *this;
+}
+
+String& String::operator=( const char  * s ){
+	len = strlen(s);
+	data = new char[len];
+	for(int i=0;i<len;i++)
+		data[i] = s[i];
+	return *this;
+}
+
+// Destructor : free some space
 String::~String(){
 	len = 0;
 	delete[] data;
@@ -52,20 +73,16 @@ String::~String(){
 __SIZETYPE String::length(){
 	return len;
 }
-// operator [] : gives char at index . returns null if out of bounds.
-String String::operator[]( __SIZETYPE index ){
-	String temp;
-	if( index < len ) return temp;
-	char ch[] = {data[index]};
-	temp = ch;
-	return temp;
-}
-char String::charAt( __SIZETYPE pos){
-	if( pos > length() ) return char(0);
-	return data[pos];
+
+// return the character at an index
+char String::charAt( __SIZETYPE index){
+	if( index < 0 ) index += len;
+	if( index > len ) return 0;
+	return data[index];
 }
 
-String& String::operator+(String r){
+// Operator +
+String& String::operator+(String& r){
 	int tlen = r.length();
 	char* tmp = new char[ len+ tlen +1];
 	for(__SIZETYPE i=0;i<len;i++)
@@ -74,28 +91,139 @@ String& String::operator+(String r){
 		tmp[i+len] = r.charAt(i);
 	tmp[len+ tlen]= '\0';
 	String* c = new String(tmp);
-	return c;
+	return *c;
 }
+String& String::operator+( const char *r){
+	int tlen = strlen(r);
+	char* tmp = new char[ len+ tlen +1];
+	for(__SIZETYPE i=0;i<len;i++)
+		tmp[i] = data[i];
+	for(__SIZETYPE i=0;i<tlen;i++)
+		tmp[i+len] = r[i];
+	tmp[len+ tlen]= '\0';
+	String* c = new String(tmp);
+	return *c;
+}
+String& String::operator+( const char c ){
+	char *s = new char[1];
+	s[0] = c;
+	return this->operator+(s);
+}
+
+// substring functions
+String& String::substr( __SIZETYPE l, __SIZETYPE u = 0 ){
+	if( l < 0 ) l += len;
+	if( u < 0 ) u += len;
+	
+	String *ret = new String();
+	if( l >= u || u > len  ) return *ret; // return null string, as bounds are erred
+	
+	char *s = new char[ u-l+1 ];
+	for( __SIZETYPE i=l; i<u ; ++i ){
+		s[i-l] = data[i];	
+	}
+	s[u-l] = '\0';
+	*ret = s;
+	return *ret;
+}
+
+// searching :
+
+// returns the index of  beginning of string s, if found, else returns -1
+__SIZETYPE String::indexOf( String& s ){
+	__SIZETYPE tlen = s.length();
+	bool isfound ;
+	for( __SIZETYPE i = 0; i < len-tlen ; ++i ){
+		isfound = true;
+		for( __SIZETYPE j = 0; j < tlen ; ++j )
+			if( s.charAt(j) != data[i] ){
+				isfound = false;
+				break;
+			}
+		if( isfound ) return i;
+	}
+	return -1;
+}
+__SIZETYPE String::indexOf( const char *s ){
+	__SIZETYPE tlen = strlen(s);
+	bool isfound ;
+	for( __SIZETYPE i = 0; i < len-tlen ; ++i ){
+		isfound = true;
+		for( __SIZETYPE j = 0; j < tlen ; ++j ){
+			if( s[j] != data[i+j] ){
+				isfound = false;
+				break;
+			}
+		}
+		if( isfound ) return i;
+	}
+	return -1;
+}
+
 
 /**********************************
-* Functions : read - write
+* Functions : write
 ***********************************/
 
-/** operator = **/
-String String::operator=( String s){
-	len = s.length();
-	//delete[] data;
-	data = new char[len];
-	for(int i=0;i<len;i++)
-		data[i] = s.charAt(i);
-	return *this;
-}
-String String::operator=( const char  * s ){
-	len = strlen(s);
-	data = new char[len];
-	for(int i=0;i<len;i++)
-		data[i] = s[i];
-	return *this;
+
+
+/**********************************
+* Functions : read/write
+**********************************/
+
+// operator [] : gives reference to char at index . returns null if out of bounds.
+char& String::operator[]( __SIZETYPE index ){
+	if( index < 0 ) index += len;
+	if( index > len ){
+		char *t = new char;
+		return *t;
+	}
+	return data[index];
 }
 
+/************************************
+* Stream IO operators : >> <<
+************************************/
+ostream& operator<<( ostream &output, String &D )
+{ 
+ 	D.printToStream( output );
+ 	return output;            
+}
+bool String::printToStream( ostream &output ){
+	for( __SIZETYPE i=0;i<len;++i)
+		output<<data[i];	
+}
+
+// this is the global input buffer for all strings.
+// as string length is not known, 
+// creating a huge buffer during each input operation is not viable.
+// so , just create a global buffer and use it for every string. ( C++ is synchronous :) )
+char *stringInputBuffer = new char[ MAX_STRING_LENGTH ];
+istream& operator>>( istream &input, String &D )
+{ 
+	cin.getline( stringInputBuffer , MAX_STRING_LENGTH );
+	D = stringInputBuffer;
+ 	return input;            
+}
+
+/*******************************
+* Compatiblity modules for string constants
+* char *
+********************************/
+String::operator char*(){
+	char *ret = new char[len+1];
+	for(__SIZETYPE i=0;i<len;i++) ret[i] = data[i];
+	ret[len] = '\0';
+	return ret;
+}
+String::operator char(){
+	return data[0];
+}
+
+//-----------------------------------------------------------
+// DEBUG - delete on completing development...
+bool String::log(){
+	printToStream( cout );
+}
+	
 
