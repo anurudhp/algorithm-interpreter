56d99bc49e50ee106a3ec6b5c6cf09fa1482a369
anurudhp <anurudhp97@gmail.com>
2014-09-28 20:27:59 +0530

    second upload: string

--- lib/codestring.h
+++ lib/codestring.h
@@ -1,19 +1,21 @@
 /*************
 * This library is an alternative for the C++ STL String template
 * which is not available on older compilers.
-* Just include this header, and happily feel that strings 
-* in older c++ compilers are just another fundamental type!
-
+*
 *	DISCLAIMER : This template does not guarentee the same performance 
 *			or memory optimization or usability as the actual C++ STL string template.
 *			This was designed only to give improved functionality to older compilers
 *
-*	note that  every operator has to be overloaded with (char*) too,
-*  so that it is compatible with Turbo C++ string constants ( c-style strings) ...
+* @license MIT License 
+*	-- @c codelegend
 **************/
-
-/************** NOTE : DEVELOPMENT IS NOT DONE YET. MODULE MAY NOT COMPILE PROPERLY *******************/
-
+/** 
+* note that  every operator has to be overloaded with (char*) too,
+*  so that it is compatible with Turbo C++ string constants ( c-style strings) ...
+* this forces overloading a function multiple times, especially when it takes more than one 'String' parameter. 
+*
+* -- there is also provision for backward compatiblity : typecasting String to char*...
+**/
 #ifndef CODE_STRING_H
 #define CODE_STRING_H
 
@@ -25,26 +27,54 @@ class String
 		__SIZETYPE len; // size
 		char* data; // the actual string, but hidden to user. see operators [] and = for usage.
 	public:
-		bool log();
-		// data
-		
 		// constructors
 		String();
 		String( String& );
 		String( const char * );
-		String operator = ( String );
-		String operator = ( const char * );
+		String& operator = ( String& );
+		String& operator = ( const char * );
 		
 		// destructor 
 		~String();
 		
 		
-		// functions : do not affect String
+		/** functions : read only **/
 		__SIZETYPE length();
-		String operator[](__SIZETYPE);
+		char& operator [] ( __SIZETYPE );
 		char charAt( __SIZETYPE );
-		String* operator + ( String );
 		
-	
+		String& operator + ( String& );
+		String& operator + ( const char* );
+		String& operator + ( const char );
+		
+		// searching
+		__SIZETYPE indexOf( String& );
+		__SIZETYPE indexOf( const char * );
+		//long countOccurences( String& );
+		//long countOccurences( const char * );
+		
+		// editing
+		String& substr( __SIZETYPE, __SIZETYPE );
+		//String& tolower();
+		//String& toupper();
+		//String& trim();
+		//String& replace( String& , String& );
+		//String& replace( String& , const char * );
+		//String& replace( const char * , String& );
+		//String& replace( const char * , const char * );
+		
+		
+		/** functions : write **/
+		
+		/** Overloads for input/output **/
+		friend istream& operator >> ( istream& , String& );
+		friend ostream& operator << ( ostream& , String& );
+		bool printToStream( ostream& );
+		bool log();
+		
+		/** Backward compatibility **/
+		operator char*();
+		operator char();
 };
-#endif /* CODE_STRING_H */ 
+
+#endif /* CODE_STRING_H */
--- lib/dependencies.h
+++ lib/dependencies.h
@@ -5,6 +5,7 @@
 // coz I cant code everything from scratch.
 
 // check compiler and include files accordingly
+
 #if defined(__GNUC__)
 
 #include <cstring>
@@ -22,13 +23,22 @@ using namespace std;
 
 #endif
 
-// Custom types and constants
 #define DEFAULT_STRING_SIZE 80
-typedef unsigned long int __SIZETYPE;
+#define MAX_STRING_LENGTH 100000
+
+// Custom types
+
+#if defined(__GNUC__)
+typedef long long int  __SIZETYPE;
+#else
+typedef long int  __SIZETYPE; // older compilers dont support long long.
+#endif
+
 
 // Custom Templates
 class String;
 template <typename> class Vector;
 template <typename> class Stack;
+
 
 #endif /* CUSTOM_DEPENDENCIES_H */
