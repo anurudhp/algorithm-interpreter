484bb74f424d02dd01578f5b20c13c8e7ce138ec
anurudhp <anurudhp97@gmail.com>
2014-12-23 01:27:12 +0530

    refactor vector, add class iterator.

--- lib/codevector.cpp
+++ lib/codevector.cpp
@@ -1,201 +1,259 @@
 // do not include this file separately
 // include codevector.h instead
 
+/*********************************************
+** Implementation : 						**
+** template class Vector					**
+**********************************************/
+
 /***********************
 * Constructors, destructor and operator =
 ************************/
 template<typename T>
 Vector<T>:: Vector(){
-
-    head = NULL;
-    tail = NULL;
+    head = tail = NULL;
     length = 0;
 }
 
 template<typename T>
-Vector<T>::Vector(const Vector<T>& v){
-
-    if(v.size()==0){
-        head=NULL;
-        tail=NULL;
-        length=0;
-    }
-
-    else{
-        length=v.size();
-        node* n=new node;
-        head=n;
-        tail=n;
-        n->value=v[0];
-        n->next=NULL;
-        n->prev=NULL;
-
-        for(__SIZETYPE i=1;i<v.size();i++){
-            n=new node;
-            tail->next=n;
-            n->prev=tail;
-            tail=n;
-            n->value=v[i];
-            n->next=NULL;
-        }
-    }
+Vector<T>::Vector(const Vector<T>& v) {
+	*this = v;
 }
+template<typename T>
+Vector<T>& Vector<T>::operator= (const Vector<T>& v) {
+	length = v.size();
+    head = tail = NULL;
+    for( __SIZETYPE i = 0; i<length; i++)
+        pushback( v[i] );
+    return *this;
+}
+
 
 template<typename T>
 Vector<T>::Vector(__SIZETYPE len,T val){
-
-    if(len==0){
-        head=NULL;
-        tail=NULL;
-        length=0;
-    }
-
-    else{
-        length=len;
-        node* n=new node;
-        head=n;
-        tail=n;
-        n->value=val;
-        n->next=NULL;
-        n->prev=NULL;
-
-        for(int i=1;i<length;i++){
-            n=new node;
-            n->value=val;
-            n->next=NULL;
-            tail->next=n;
-            n->prev=tail;
-            tail=n;
-        }
-    }
+	length = len;
+    head = tail = NULL;
+    for( __SIZETYPE i = 0; i<length; i++)
+		pushback(val);
 }
-
 template<typename T>
-Vector<T>::~Vector(){
-    node* pointer=head;
-    node* next;
-    for(int i=0;i<length;i++){
-        next=pointer->next;
-        delete pointer;
-        pointer=next;
-    }
+bool Vector<T>::clear() {
+    while( popback() );
+    return true;
+}
+template<typename T>
+Vector<T>::~Vector() {
+    clear();
 }
 
-/*******************************
+/*********************
 * Properties
-******************************/
+**********************/
 
-// size() : returns the size/length of the vector
 template<typename T>
-__SIZETYPE Vector<T>::size()const{
+__SIZETYPE Vector<T>::size() const {
     return length;
+}
+template<typename T>
+bool Vector<T>::isEmpty() const {
+    return (length == 0);
 }
 
 // operator[] : returns reference to value at index
 template<typename T>
-T& Vector<T>::operator[](__SIZETYPE index) const {
-    node* pointer = head;
-    for(int i = 0; i < index; i++ )
+T& Vector<T>::operator[] (__SIZETYPE index) const {
+    node *pointer = head;
+    while( index-- )
         pointer = pointer->next;
     return pointer->value;
 }
 
-/************************************
+/**************************
 * Basic manipulations
-*************************************/
-template<typename T>
-Vector<T>& Vector<T>::pushback(T val){
+***************************/
 
-    length++;
-    node* n=new node;
-    n->value=val;
-    n->next=NULL;
-    n->prev=tail;
-    if(tail!=NULL) tail->next=n;
-    tail=n;
-    if(length==1) head=n;
+template<typename T>
+Vector<T>& Vector<T>::pushback( const T& val){
+    node *n = NULL;
+	n = new node;
+	if(!n) forcequit("vector|heap exhausted");
+	
+	length++;
+    n->value = val;
+    n->next = NULL;
+    n->prev = tail;
+    if(tail != NULL) tail->next = n;
+    tail = n;
+    if(length == 1) head = n;
     return *this;
 }
 
 template<typename T>
-Vector<T>& Vector<T>::pushfront(T val){
-
-    length++;
-    node* n=new node;
-    n->value=val;
-    n->next=head;
-    n->prev=NULL;
-    if(head!=NULL) head->prev=n;
-    head=n;
-    if(length==1) tail=head;
+Vector<T>& Vector<T>::pushfront( const T& val){
+	node *n = NULL;
+	n = new node;
+	if(!n) forcequit("vector|heap exhausted");
+	
+	length++;
+    n->value = val;
+    n->next = head;
+    n->prev = NULL;
+    if(head != NULL) head->prev = n;
+    head = n;
+    if(length == 1) tail = head;
     return *this;
 }
 
 template<typename T>
-Vector<T>& Vector<T>::insert(T val, __SIZETYPE index){
-
-    if(index<0||index>length) return *this;
-    length++;
-    node* n=new node;
-    n->value=val;
-    bool check=false;
-    if(index==0){
-        n->next=head;
-        n->prev=NULL;
-        head=n;
-        check=true;
-    }
-    if(index==length-1){
-        tail->next=n;
-        n->prev=tail;
-        tail=n;
-        tail->next=NULL;
-        check=true;
-    }
-    if(check==true) return *this;
-    node* pointer=head;
-    for(int i=1;i<index;i++) pointer=pointer->next;
-    pointer->next->prev=n;
-    n->next=pointer->next;
-    pointer->next=n;
-    n->prev=pointer;
-    return *this;
-}
-
-template<typename T>
-void Vector<T>::popback(){
-
-    if(length==1){
+bool Vector<T>::popback(){
+	if(length == 0) return false;
+	
+    if(length == 1) {
         delete head;
-        head=NULL;
-        tail=NULL;
-        length=0;
-        return;
-    }
-    length--;
-    node*pointer=tail->prev;
-    delete tail;
-    tail=pointer;
-    tail->next=NULL;
-    return;
+        head = tail = NULL;
+    } else {
+	    node *n = tail->prev;
+	    delete tail;
+	    tail = n;
+	    tail->next = NULL;
+	}
+	length--;
+    return true;
 }
 
 template<typename T>
-void Vector<T>::popfront(){
-
-    if(length==1){
+bool Vector<T>::popfront() {
+	if(length == 0) return false;
+	
+    if(length==1) {
         delete head;
-        head=NULL;
-        tail=NULL;
-        length=0;
-        return;
-    }
-    length--;
-    node* newhead;
-    newhead=head->next;
-    delete head;
-    head=newhead;
-    head->prev=NULL;
-    return;
+        head = tail = NULL;
+    } else {
+	    node *newhead;
+	    newhead = head->next;
+	    delete head;
+	    head = newhead;
+	    head->prev = NULL;
+	}
+	length--;
+    return true;
+}
+
+template<typename T>
+Vector<T>& Vector<T>::insert( __SIZETYPE index, const T& val){
+    if (index<0 || index>length) return *this;
+    iterator it = begin();
+    while( index-- && it++ );
+    return insert( it, val);
+}
+
+// insert using an iterator.
+template<typename T>
+Vector<T>& Vector<T>::insert(iterator& it, const T& val) {
+	if( it.ptr->next == NULL ) return pushback(val);
+	
+    length++;
+    node *n = new node;
+    if(!n) forcequit("vector|heap exhausted");
+    
+    node *oldnext = it.ptr->next;
+    n->value = val;
+    n->prev = it.ptr;
+    n->next = oldnext;
+    
+    it.ptr->next = n;
+    oldnext->prev = n;
+    
+    return *this;
+}
+
+// iterator constants for a vector
+template <typename T>
+typename Vector<T>::iterator Vector<T>::begin() const {
+	Vector<T>::iterator it;
+	it.ptr = this->head;
+	return it;
+}
+template <typename T>
+typename Vector<T>::iterator Vector<T>::end() const {
+	Vector<T>::iterator it;
+	it.ptr = this->tail;
+	return it;
+}
+// end implementation : Vector
+
+
+/******************************************
+** Implementation :                      **
+** template class Vector::iterator       **
+******************************************/
+
+// constructors and operator =
+// there is no need for a custom destructor, as all data in iterator is 'static'
+template <typename T>
+Vector<T>::iterator::iterator() {
+	this->ptr = NULL;
+}
+
+template <typename T>
+Vector<T>::iterator::iterator(const iterator& it) {
+	this->ptr = it.ptr;
+}
+
+template <typename T>
+typename Vector<T>::iterator Vector<T>::iterator::operator= (const iterator& it) {
+	this->ptr = it.ptr;
+}
+
+template <typename T>
+Vector<T>::iterator::iterator(const Vector<T>& vec, __SIZETYPE len) {
+	*this = vec.begin();
+	while( len-- && this->operator++() );
+}
+
+// mutators
+template <typename T>
+T& Vector<T>::iterator::operator* () const {
+	if( ptr == NULL ){
+		T v;
+		return v;
+	}
+	return ptr->value;
+}
+template <typename T>
+bool Vector<T>::iterator::operator++ () {
+	if( ptr == NULL ) return false;
+	ptr = ptr->next;
+	return !!ptr;
+}
+template <typename T>
+bool Vector<T>::iterator::operator-- () {
+	if( ptr == NULL ) return false;
+	ptr = ptr->prev;
+	return !!ptr;
+}
+template <typename T>
+bool Vector<T>::iterator::operator++ (int) {
+	operator++();
+	return !!ptr;
+}
+template <typename T>
+bool Vector<T>::iterator::operator-- (int) {
+	operator--();
+	return !!ptr;
+}
+
+// relational operators
+template <typename T>
+bool Vector<T>::iterator::operator! () const {
+	return ( this->ptr == NULL );
+}
+template <typename T>
+bool Vector<T>::iterator::operator== ( const Vector<T>::iterator& it ) const {
+	return ( this->ptr == it.ptr );
+}
+template <typename T>
+bool Vector<T>::iterator::operator!= ( const Vector<T>::iterator& it ) const {
+	return ( this->ptr != it.ptr );
 }
 
