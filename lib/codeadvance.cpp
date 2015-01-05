6b35c0b2e050133b4190005302bf99c43abd1496
anurudhp <anurudhp97@gmail.com>
2015-01-05 15:16:46 +0530

    slightly modify strsplit and join

--- lib/codeadvance.cpp
+++ lib/codeadvance.cpp
@@ -1,26 +1,35 @@
 #include "codestring.h"
 #include "codevector.h"
 
+// forward declarations:
+Vector<String> strsplit(String, String);
+String strjoin(Vector<String>, String);
+
+/** Implementation **/
 // splits a string into parts, by spl.
-Vector<String> strsplit(const String& str, const String& spl) {
+Vector<String> strsplit(String str,  String spl = "") {
 	Vector<String> ret;
-	String tmp = str;
-	if (!spl) return ret;
+	if (!spl) {
+		// splits it into single characters.
+		for (__SIZETYPE i = 0; i < str.length(); i++)
+			ret.pushback(str[i]);
+		return ret;
+	}
 	__SIZETYPE index;
-	while (!!tmp) {
-		index = tmp.indexOf(spl);
+	while (!!str) {
+		index = str.indexOf(spl);
 		if (index == -1) {
-			ret.pushback(tmp);
+			ret.pushback(str);
 			return ret;
 		}
-		ret.pushback(tmp.substr(0, index));
-		tmp = tmp.substr(index + spl.length());
+		ret.pushback(str.substr(0, index));
+		str = str.substr(index + spl.length());
 	}
 	return ret;
 }
 
 // joins a vector of strings, concatenating join as a punctuator.
-String strjoin(Vector<String> vs, const String& join) {
+String strjoin(Vector<String> vs, String join = "") {
 	String ret;
 	bool first = true;
 	for (__SIZETYPE i = 0; i < vs.size(); i++) {
