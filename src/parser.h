3c96ef705ed2d3a0a64737d789997685b8570866
blazikenmask <blazikenmask@gmail.com>
2015-01-24 19:21:14 +0530

    move class variable to separate header

--- src/parser.h
+++ src/parser.h
@@ -29,69 +29,9 @@ class Error
 idList errorCodes, errorDesc;
 bool importErrorCodes();
 
-typedef Queue<Token> Infix;
 typedef Queue<Token> RPN;
 
-class Variable
-{
-	String _id;
-	Token _value;
-	Vector<Token> _keys;
-	Vector<Variable> _values;
-
-	public:
-	Variable();
-	Variable(Token);
-	Variable(const Variable&);
-	Variable& operator= (const Variable&);
-
-	String id() const;
-	tokenType type();
-	Token value() const;
-	Variable& valueAt(Token);
-	bool setValue(const Variable&);
-	bool setValueAt(Token, Variable);
-};
-
-class VariableScope
-{
-	Vector< Vector<Variable> > varstack;
-
-	public:
-	VariableScope();
-	VariableScope(const VariableScope&);
-	
-	bool stackVariables();
-	bool stackVariables(Vector<Variable>&);
-	bool addVariable(Variable&);
-	bool popVariables();
-	bool popVariables(Vector<Variable>&);
-	Variable resolve(String);
-	__SIZETYPE exists(String);
-	__SIZETYPE depth() const;
-};
-
-class Function
-{
-	String _id;
-	Vector<String> params;
-	RPN statements;
-
-	public:
-	Function();
-	Function(String);
-	Function(String, Vector<String>, RPN);
-	Function(const Function&);
-
-	__SIZETYPE paramsSize() const;
-	String id() const;
-	RPN getStatements() const;
-
-	bool setParams(Vector<String>);
-	bool setStatements(RPN);
-	bool validate();
-	Token evaluate(Vector<Token>, Evaluator&);
-};
+#include "variables.h"
 
 // parser status.
 #define SUCCESS 41
