6d3d61c60fad94a017183b7a5036550450d1e6a4
blazikenmask <blazikenmask@gmail.com>
2015-01-24 17:44:48 +0530

    add operator priority comparing function

--- src/operations.cpp
+++ src/operations.cpp
@@ -14,16 +14,33 @@ namespace Operations {
 	
 	Token compare(String, Token, Token);
 	Token logical(String, Token, Token);
+	
+	int priority(String);
+	int comparePriority(String, String);
 };
 
-Token Operations::mathOperator(String op, Token a, Token b) {
+Token Operations::mathOperator(String op, Token a, Token b) {/*
 	if (op == "+") return add(a, b);
 	if (op == "-") return subtract(a, b);
 	if (op == "*") return multiply(a, b);
 	if (op == "/") return divide(a, b);
 	if (op == "%") return modulo(a, b);
-	
+	*/
 	return nullval;
+}
+
+int Operations::priority(String op) {
+	if (op == ".") return 6;
+	if (op == "++" || op == "--") return 5;
+	if (op == "*" || op == "/" || op == "%")  return 4;
+    if (op == "+" || op == "-") return 3;
+    if (op.substr(0,2) == "==" || op.substr(0, 2) == "!=" || op[0] == '<' || op[0] == '>') return 2;
+    if (op == "!" || op == "&&" || op == "||" || op == "=" || op[1] == '=') return 1;
+    return 0;
+}
+
+int Operations::comparePriority(String a, String b) {
+	return priority(a) - priority(b);
 }
 #endif /* COMPONENT_PARSER_OPERATION_H */
 #endif /* COMPONENT_PARSER_H */
