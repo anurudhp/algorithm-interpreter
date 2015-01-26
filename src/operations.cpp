64e4b0f1d7f16619b5c9d1558353e5afc57c3907
anurudhp <anurudhp97@gmail.com>
2015-01-26 23:26:29 +0530

    error fixes in operations
    - fixed logical inconsistency in type check for modulo
    - fixed pre-processor directive ifdef without a #
    - moved the functions
    - activated mathOperator

--- src/operations.cpp
+++ src/operations.cpp
@@ -1,4 +1,4 @@
-ifdef COMPONENT_PARSER_H
+#ifdef COMPONENT_PARSER_H
 #ifndef COMPONENT_PARSER_OPERATION_H
 #define COMPONENT_PARSER_OPERATION_H
 
@@ -17,19 +17,62 @@ namespace Operations {
 
 	int priority(String);
 	int comparePriority(Token, Token);
-
 };
 
-Token Operations::mathOperator(String op, Token a, Token b) {/*
+Token Operations::mathOperator(String op, Token a, Token b) {
 	if (op == "+") return add(a, b);
 	if (op == "-") return subtract(a, b);
 	if (op == "*") return multiply(a, b);
 	if (op == "/") return divide(a, b);
 	if (op == "%") return modulo(a, b);
-	*/
 	return nullval;
 }
 
+Token Operations::add(Token t1, Token t2) {
+    if (t1.subtype() != t2.subtype()) return nullToken;
+    if (t1.subtype() == NUMBER) {
+        double a1 = t1.value().toNumber(),
+		       a2 = t2.value().toNumber();
+        return Lexer::toToken(numberToString(a1 + a2));
+    }
+    if (t1.subtype() == STRING) {
+        String s1 = Lexer::tokenToString(t1),
+		       s2 = Lexer::tokenToString(t2);
+        return Lexer::toToken(s1 + s2);
+    }
+}
+
+Token Operations::subtract(Token t1, Token t2) {
+    if (t1.subtype() != NUMBER && t2.subtype() != NUMBER) return nullToken;
+    double a1 = t1.value().toNumber(),
+	       a2 = t2.value().toNumber();
+    return Lexer::toToken(numberToString(a1 - a2));
+}
+
+Token Operations::multiply(Token t1, Token t2) {
+    if (t1.subtype() != NUMBER && t2.subtype() != NUMBER) return nullToken;
+    double a1 = t1.value().toNumber(),
+	       a2 = t2.value().toNumber();
+    return Lexer::toToken(numberToString(a1 * a2));
+}
+
+Token Operations::divide(Token t1, Token t2) {
+    if (t1.subtype() != NUMBER && t2.subtype() != NUMBER) return nullToken;
+    double a1 = t1.value().toNumber(),
+	       a2 = t2.value().toNumber();
+    if (a2 == 0.0) return nullToken;
+    return Lexer::toToken(numberToString(a1 / a2));
+}
+
+Token Operations::modulo(Token t1, Token t2) {
+    if (!(t1.value().isInteger() && t2.value().isInteger())) return nullToken;
+    long a1 = t1.value().toInteger(),
+	     a2 = t2.value().toInteger();
+    if (a2 == 0) return nullToken;
+    return Lexer::toToken(integerToString(a1 % a2));
+}
+
+// Operator Priorities:
 int Operations::priority(String op) {
 	if (op == ".") return 6;
 	if (op == "++" || op == "--") return 5;
@@ -43,46 +86,6 @@ int Operations::priority(String op) {
 int Operations::comparePriority(Token a, Token b) {
 	return priority(a.value()) - priority(b.value());
 }
-
-Token Operations::add(Token t1, Token t2) {
-    if(t1.subtype() != t2.subtype() ) return nullToken;
-    if(t1.subtype() == NUMBER) {
-        double a1 = t1.value().toNumber(),a2 = t2.value().toNumber();
-        return Lexer::toToken(numberToString(a1 + a2));
-    }
-    if(t1.subtype() == STRING) {
-        String s1 = Lexer::tokenToString(t1),s2 = Lexer::tokenToString(t2);
-        return Lexer::toToken(s1 + s2);
-    }
-}
-
-Token Operations::subtract(Token t1, Token t2) {
-    if(t1.subtype() != NUMBER && t2.subtype() != NUMBER) return nullToken;
-    double a1 = t1.value().toNumber(),a2 = t2.value().toNumber();
-    return Lexer::toToken(numberToString(a1 - a2));
-}
-
-Token Operations::multiply(Token t1, Token t2) {
-    if(t1.subtype() != NUMBER && t2.subtype() != NUMBER) return nullToken;
-    double a1 = t1.value().toNumber(),a2 = t2.value().toNumber();
-    return Lexer::toToken(numberToString(a1 * a2));
-}
-
-Token Operations::divide(Token t1, Token t2) {
-    if(t1.subtype() != NUMBER && t2.subtype() != NUMBER) return nullToken;
-    double a1 = t1.value().toNumber(),a2 = t2.value().toNumber();
-    if(a2==0.0) return nullToken;
-    return Lexer::toToken(numberToString(a1 / a2));
-}
-
-Token Operations::modulo(Token t1, Token t2) {
-    if((!t1.value().isInteger()) && (!t2.value().isInteger())) return nullToken;
-    long a1 = t1.value().toInteger(),a2 = t2.value().toInteger();
-    if(a2==0) return nullToken;
-    return Lexer::toToken(integerToString(a1 % a2));
-}
-
-
 
 #endif /* COMPONENT_PARSER_OPERATION_H */
 #endif /* COMPONENT_PARSER_H */
