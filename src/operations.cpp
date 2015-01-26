0e73b73f053bfeccbb841bc4643f6eca1b36038e
blazikenmask <blazikenmask@gmail.com>
2015-01-26 21:11:39 +0530

    remove deprecated functions
    - in namespace operations.

--- src/operations.cpp
+++ src/operations.cpp
@@ -16,12 +16,7 @@ namespace Operations {
 	Token logical(String, Token, Token);
 
 	int priority(String);
-	int comparePriority(String, String);
-
-    bool isInteger(Token);
-	double toNumber(Token);
-	String toString(Token);
-	long toInteger(Token);
+	int comparePriority(Token, Token);
 
 };
 
@@ -39,54 +34,14 @@ int Operations::priority(String op) {
 	if (op == ".") return 6;
 	if (op == "++" || op == "--") return 5;
 	if (op == "*" || op == "/" || op == "%")  return 4;
-    if (op == "+" || op == "-") return 3;
-    if (op.substr(0,2) == "==" || op.substr(0, 2) == "!=" || op[0] == '<' || op[0] == '>') return 2;
-    if (op == "!" || op == "&&" || op == "||" || op == "=" || op[1] == '=') return 1;
-    return 0;
+	if (op == "+" || op == "-") return 3;
+	if (op.substr(0,2) == "==" || op.substr(0, 2) == "!=" || op[0] == '<' || op[0] == '>') return 2;
+	if (op == "!" || op == "&&" || op == "||" || op == "=" || op[1] == '=') return 1;
+	return 0;
 }
 
-int Operations::comparePriority(String a, String b) {
-	return priority(a) - priority(b);
-}
-
-bool Operations::isInteger(Token t) {
-    if(t.value().indexOf(".") == -1) return true;
-    return false;
-}
-
-double Operations::toNumber(Token t) {
-    if(t.subtype() != NUMBER) return 0.0;
-    double val = 0.0,beginning = 0;
-    if(t.value()[0] == '-') beginning = 1;
-    if(isInteger(t)){
-        for(int i = t.value().length()-1, j = 1;i >= beginning;i--, j *= 10) {
-            val += double((t.value()[i] - '0') * j);
-        }
-        if(beginning == 1) val *= -1.0;
-        return val;
-    }
-    else{
-        long long pos=t.value().indexOf(".");
-        for(int i = pos-1, j = 1;i >= beginning;i--, j *= 10) {
-            val += double((t.value()[i] - '0') * j);
-        }
-        double j = 0.1;
-        for(int i = pos+1;i < t.value().length();i++, j /= 10.0){
-            val += double(t.value()[i]-'0') * j;
-        }
-        if(beginning == 1) val *= -1.0;
-        return val;
-    }
-}
-
-long Operations::toInteger(Token t) {
-    if(!isInteger(t)) return 0;
-    return int(toNumber(t));
-}
-
-String Operations::toString(Token t) {
-    if(t.subtype() != STRING) return "";
-    return t.value().substr(1, t.value().length() - 2);
+int Operations::comparePriority(Token a, Token b) {
+	return priority(a.value()) - priority(b.value());
 }
 
 #endif /* COMPONENT_PARSER_OPERATION_H */
