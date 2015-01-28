8b0349af6c8c88a397ec3e0cc963109b12049d2c
anurudhp <anurudhp97@gmail.com>
2015-01-28 12:29:18 +0530

    fix calls to Vector<String>::indexOf
    pass second arg (comparator)

--- lib/codeadvance.cpp
+++ lib/codeadvance.cpp
@@ -6,6 +6,7 @@ Vector<String> strsplit(String, String);
 String strjoin(Vector<String>, String);
 String integerToString(long);
 String numberToString(double);
+bool stringEquals(String, String);
 
 /** Implementation **/
 // splits a string into parts, by spl.
@@ -55,4 +56,8 @@ String numberToString(double val) {
 	char str[100];
 	sprintf(str, "%f", val);
 	return String(str);
+}
+
+bool stringEquals(String a, String b) {
+	return (a.equals(b));
 }
