8981384bd800d9ac18b898f2ba40eb5935e20983
anurudhp <anurudhp97@gmail.com>
2015-01-31 00:35:42 +0530

    fully refactor variables

--- src/variables.cpp
+++ src/variables.cpp
@@ -24,11 +24,17 @@ Variable& Variable::operator= (const Variable& v) {
 	return *this;
 }
 
-String Variable::id() const { return this->_id; }
+String Variable::id() const {
+	return this->_id;
+}
 
 tokenType Variable::type() {
 	tokenType ty = this->_value.type();
-	if (ty == NUMBER || ty == STRING || ty == BOOLEAN) return ty;
+	switch (ty) {
+		case NUMBER: case BOOLEAN: case STRING:
+		case HASHED: case ARRAY: case OBJECT:
+		return ty;
+	}
 	if (!this->_keys.empty()) return OBJECT;
 	if (!this->_values.empty()) return ARRAY;
 	return UNKNOWN;
@@ -37,36 +43,87 @@ tokenType Variable::type() {
 Token Variable::value() const { return this->_value; }
 
 // Member/embedded value accessors
-
-tokenType Variable::hasMember(Token ref) {
-	if (ref.type() != LITERAL) return false;
-	if (this->type() == ARRAY) {
-		if (ref.subtype() == NUMBER && ref.value().isInteger()) {
-			long index = ref.value().toInteger();
-			if (index >= 0 && index <= this->_values.size()) return true;
-			return false;
-		}
-		return false;
-	}
-	else if (this->type() == OBJECT) {
-		if (this->_keys.indexOf(ref.value()) >= 0) return true;
-		return false;
-	}
-	return false;
-}
-
-Variable& Variable::valueAt(Token) {
-	return *this;
-}
-
 bool Variable::setValue(const Variable& v) {
 	_value = v._value;
 	_keys = v._keys;
 	_values = v._values;
+	_object = v._object;
 	return true;
 }
-bool Variable::setValueAt(Token, Variable) {
+
+bool Variable::hasValueAt(Token key) {
+	if (this->type() == ARRAY) {
+		key = Operations::typecastToken(key, NUMBER);
+		if (key.value() == "null" || !key.value().isInteger()) return false;
+		
+		__SIZETYPE index = key.value().toInteger();
+		if (index < 0) index += this->_values.size();
+		
+		if (index < 0 || index >= this->_values.size()) return false;
+		return true;
+	}
+	else if (this->type() == OBJECT) {
+		key = Operations::typecastToken(key, STRING);
+		if (key.value() == "null") return false;
+		
+		if (this->_keys.indexOf(key.value()) == -1) return false;
+		return true;
+	}
 	return false;
+}
+
+Variable& Variable::valueAt(Token key) {
+	if (!this->hasValueAt(key)) return nullVariableRef;
+	
+	__SIZETYPE index = 0;
+	if (this->type() == ARRAY) {
+		key = Operations::typecastToken(key, NUMBER);
+		index = key.value().toInteger();
+		if (index < 0) index += this->_values.size();
+	}
+	else if (this->type() == OBJECT) {
+		key = Operations::typecastToken(key, STRING);		
+		index = _keys.indexOf(key.value());
+	}
+	return this->_values[index];
+}
+
+bool Variable::setValueAt(Token key, Variable value) {
+	if (this->type() == ARRAY) {
+		key = Operations::typecastToken(key, NUMBER);
+		if (key.value() == "null" || !key.value().isInteger()) return false;
+		
+		__SIZETYPE index = key.value().toInteger();
+		if (index < 0) index += this->_values.size();
+		
+		if (index < 0 || index > this->_values.size()) return false;
+		if (index == this->_values.size()) {
+			this->_values.pushback(Variable());
+		}
+		this->_values[index].setValue(value);
+		return true;
+	}
+	else if (this->type() == OBJECT) {
+		key = Operations::typecastToken(key, STRING);
+		if (key.value() == "null") return false;
+		
+		__SIZETYPE index = _keys.indexOf(key.value());
+		if (index == -1) { // new key
+			_keys.pushback(key.value());
+			_values.pushback(Variable());
+			_values[-1].setValue(value);
+		}
+		else { // update existing value
+			_values[index].setValue(value);
+		}
+		return true;
+	}
+	return false;
+}
+
+Function Variable::getMethod(String funcId) {
+	if (this->type() != OBJECT || this->_object == NULL) return Function();
+	return this->_object->getPrototype(funcId);
 }
 
 /*******************************
@@ -80,7 +137,7 @@ bool Variable::setValueAt(Token, Variable) {
 VariableScope::VariableScope() {
 	this->varstack.clear();
 }
-VariableScope::VariableScope(const VariableScope& sc) {
+VariableScope& VariableScope::operator= (const VariableScope& sc) {
 	this->varstack = sc.varstack;
 }
 
@@ -95,6 +152,7 @@ bool VariableScope::stackVariables(Vector<Variable>& vars) {
 
 // adds a variable to the top-most vector of the scope
 bool VariableScope::addVariable(Variable& var) {
+	if (this->varstack.size() == 0) this->stackVariables();
 	this->varstack[-1].pushback(var);
 }
 // deletes the topmost variable vector.
@@ -106,30 +164,42 @@ bool VariableScope::popVariables(Vector<Variable>& vars) {
 	return this->varstack.popback(vars);
 }
 
-// finds a variable of identifier `id` and returns it.
-// returns a null variable, if not found.
-Variable VariableScope::resolve(String id) {
-	__SIZETYPE i, j, d = this->depth();
-	for (i = d - 1; i >= 0; i--) {
-		Vector<Variable>& vars = this->varstack[i];
-		for (j = 0; j < vars.size(); j++) {
-			if (vars[j].id() == id) return vars[j];
-		}
-	}
-	return Variable();
-}
-
 // checks whether a variable of identifier `id` exists in the scope.
-__SIZETYPE VariableScope::exists(String id) {
+bool VariableScope::exists(String id) {
 	__SIZETYPE i, j, d = this->depth();
 	for (i = d - 1; i >= 0; i--) {
 		Vector<Variable>& vars = this->varstack[i];
 		for (j = 0; j < vars.size(); j++) {
-			// cerr << ">> " << vars[j].id() << endl;
 			if (vars[j].id() == id) return true;
 		}
 	}
 	return false;
+}
+bool VariableScope::existsAtTop(String id) {
+	Vector<Variable>& vars = this->varstack[-1];
+	for (__SIZETYPE j = 0; j < vars.size(); j++) {
+		if (vars[j].id() == id) return true;
+	}
+	return false;
+}
+// finds a variable of identifier `id` and returns it.
+// returns a null variable, if not found.
+Variable& VariableScope::resolve(String id) {
+	__SIZETYPE i, j, d = this->depth();
+	for (i = d - 1; i >= 0; i--) {
+		Vector<Variable>& vars = this->varstack[i];
+		for (j = 0; j < vars.size(); j++) {
+			if (vars[j].id() == id) {
+				return vars[j];
+			}
+		}
+	}
+	return nullVariableRef;
+}
+
+Vector<Variable>& VariableScope::getBaseVariables() {
+	if (this->varstack.size() == 0) this->stackVariables();
+	return this->varstack[0];
 }
 
 // scope depth.
@@ -151,31 +221,79 @@ Function::Function(String id) {
 }
 Function::Function(String id, Vector<String> params, RPN st) {
 	this->_id = id;
-	this->params = params;
+	this->parameters = params;
 	this->statements = st;
 }
 Function::Function(const Function& f) {
 	this->_id = f._id;
-	this->params = f.params;
+	this->parameters = f.parameters;
 	this->statements = f.statements;
 }
 
-__SIZETYPE Function::paramsSize() const { return this->params.size(); }
+__SIZETYPE Function::paramsSize() const { return this->parameters.size(); }
 
 String Function::id() const { return this->_id; }
 RPN Function::getStatements() const { return this->statements; }
 
-bool Function::setParams(Vector<String> p) { this->params = p; return true; }
-bool Function::setStatements(RPN st) { this->statements = st; return true; }
+bool Function::setParams(Vector<String> p) {
+	this->parameters = p;
+	return true;
+}
+bool Function::setStatements(RPN st) {
+	this->statements = st;
+	return true;
+}
 
 bool validate() {
-	return false;
 }
-Token Function::evaluate(Vector<Token>, Evaluator&) {
-	return Token();
+Token Function::evaluate(Vector<Token> parameters, Evaluator& eval) {
+	for (__SIZETYPE i = 0; i < functionVariables.size(); i++) {
+		if (i > parameters.size()) parameters.pushback(nullvalToken);
+		functionVariables[i].setValue(Variable(parameters[i]));
+	}
+	VariableScope scope;
+	scope.stackVariables(eval.getGlobals());
+	scope.stackVariables(functionVariables);
+
+	Token ret = eval.evaluateRPN(this->statements, scope);
+	
+	// update the globals in the evaluator.
+	eval.getGlobals() = scope.getBaseVariables();
+	
+	if (!ret.value()) ret = nullvalToken;
+	return ret;
 }
 /*******************************
 * End Implementation: Function 
 ********************************/
+
+/*******************************
+* Implementation: class Object 
+********************************/
+
+String Object::id() { return this->_id; }
+
+Function Object::getConstructor() {
+	return this->constructor;
+}
+
+bool Object::hasPrototype(String funcid) {
+	for (__SIZETYPE i = 0; i < this->prototypes.size(); i++) {
+		if (this->prototypes[i].id() == funcid) return true;
+	}
+	return false;
+}
+Function Object::getPrototype(String funcid) {
+	for (__SIZETYPE i = 0; i < this->prototypes.size(); i++) {
+		if (this->prototypes[i].id() == funcid) return this->prototypes[i];
+	}
+	return Function();
+}
+
+Variable Object::construct(Vector<Variable> initArgs) {
+	Variable v;
+	v._object = this;
+	return v;
+}
 
 #endif /* COMPONENT_PARSER_H */
